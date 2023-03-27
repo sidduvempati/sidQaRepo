a61c89a3e5ed58ded2163aaa4e30895c332b0c69
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-03-27 13:43:42 +0000

    3

---
+++ vars+datatypes.py
@@ -0,0 +1,70 @@
+# Variables - a reference - the name of a variable
+# - a section of memory
+# - reserved for storing data - can only be altered if \
+#   called be name.
+
+# naming convention - name should be descriptive, start with lowercase and not a number
+
+#x = "5" 
+#var = 5
+#my_var_is_easy_to_read = 5
+#myVarIsEasyToRead = 5 
+
+#print = 5 # dont use reserved words
+#1var = 5 #  dont start with a number
+#Var = 5 # dont start with a capital
+#VAR = 5 # not in caps
+
+#print(x)
+
+#built in functions:
+# print() data displayed to standard output
+# input() user inputed text from standard input, string by default 
+# type() the data type of a variable 
+
+#name = input("name?: ")
+#print("your name is" + " " + name)
+#print("your name is", name)
+#print(f"your name is {name}")
+#print(type(name))
+
+#age = input("age?: ")
+#age1 = int(input("age?: "))
+
+#print(type(age))
+#print(type(age1))
+
+# four main data types in Python:
+# String: plain text, used to display info. Word, sentence, paragraph. str means string.
+# Integers: a whole number
+# FLoat: a decimal number.
+# Boolean: True or False. Case sensitive. Equivalent to 0 and 1.   
+
+# string
+#first_name = "steve"
+# Integer
+#age = 32
+# Float
+#wallet = 29.99
+
+#name = "rex"
+#age = 3
+#bark = True
+#meow = False
+
+#print("statemnt: ", name, "barks: ", bark)
+
+# Escape characters
+# \\ backslash, \n newline, \t tab u\ or U\ unicode or extended unicode
+#print("my name is \"chris\"")
+
+#print("person 1: \tHey, how are you?\nPerson2: \tGood thanks! \U0001F604")
+
+# string methods() - a string in an inbuilt class so it has methods
+
+print("Hello World".lower())
+print("hello world".upper())
+print("hello world".replace("world", "class"))
+print("hello world".split())
+print("hello world".count("l"))
+
